196e4e861df8b1672692e7b25e2c6936ab0084cd
simplyuncool <ulodasewss@gmail.com>
2026-08-24 13:53:58 +0300

    feat: redesign aurelianOS shell. Big stuff coming

---
+++ bootc/shell/build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    slint_build::compile("ui/main.slint").unwrap();
+
+    println!("cargo:rerun-if-changed=ui/main.slint");
+    println!("cargo:rerun-if-changed=ui/theme.slint");
+    println!("cargo:rerun-if-changed=ui/panel.slint");
+    println!("cargo:rerun-if-changed=ui/dock.slint");
+    println!("cargo:rerun-if-changed=ui/launcher.slint");
+
+    println!("cargo:rerun-if-changed=../../../assets/wallpaper.jpg");
+    println!("cargo:rerun-if-changed=../../../assets/icons");
+
+    println!("cargo:rerun-if-changed=../../../assets/fonts/Inter-Regular.ttf");
+    println!("cargo:rerun-if-changed=../../../assets/fonts/Inter-Medium.ttf");
+    println!("cargo:rerun-if-changed=../../../assets/fonts/Inter-SemiBold.ttf");
+}
